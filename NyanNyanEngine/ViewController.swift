a36b0920c68355853a84bd78f9a3cb3105ec70c7
Tetsuya Nishikawa <tetphi1dev@gmail.com>
2019-04-28 09:15:39 +0900

    initial commit

---
+++ NyanNyanEngine/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NyanNyanEngine
+//
+//  Created by Tetsuya Nishikawa on 2019/04/28.
+//  Copyright © 2019 Tetsuya Nishikawa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ NyanNyanEngineTests/NyanNyanEngineTests.swift
@@ -0,0 +1,34 @@
+//
+//  NyanNyanEngineTests.swift
+//  NyanNyanEngineTests
+//
+//  Created by Tetsuya Nishikawa on 2019/04/28.
+//  Copyright © 2019 Tetsuya Nishikawa. All rights reserved.
+//
+
+import XCTest
+@testable import NyanNyanEngine
+
+class NyanNyanEngineTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
