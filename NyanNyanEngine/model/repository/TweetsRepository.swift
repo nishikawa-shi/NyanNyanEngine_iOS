b811783a4ff40c9d5476b4f6c09195061b4754ec
Tetsuya Nishikawa <tetphi1dev@gmail.com>
2019-05-01 09:11:50 +0900

    Repositoryが通信処理の実装を知らなくて良いようにした

--- NyanNyanEngine/model/repository/TweetsRepository.swift
+++ NyanNyanEngine/model/repository/TweetsRepository.swift
@@ -7,7 +7,6 @@
 //
 
 import Foundation
-import Alamofire
 import RxSwift
 
 protocol BaseTweetsRepository: AnyObject {
@@ -17,24 +16,16 @@ protocol BaseTweetsRepository: AnyObject {
 class TweetsRepository: BaseTweetsRepository {
     static let shared = TweetsRepository()
     
-    private init() { }
+    private let apiClient: BaseApiClient
+    
+    private init(apiClient: BaseApiClient = ApiClient.shared) {
+        self.apiClient = apiClient
+    }
     
     func getHomeTimeLine() -> Observable<[Status]?> {
-        return self.getResponse(url: "https://nyannyanengine-ios-d.firebaseapp.com/1.1/statuses/home_timeline.json")
-            .map { [unowned self] in self.toResponseBody(dataResponse: $0) }
+        return self.apiClient
+            .getResponse(url: "https://nyannyanengine-ios-d.firebaseapp.com/1.1/statuses/home_timeline.json")
             .map { [unowned self] in self.toStatuses(data: $0) }
-    }
-    
-    private func getResponse(url: String) -> Observable<DataResponse<String>> {
-        return Observable<DataResponse<String>>.create { observer in
-            Alamofire.request(url, method: .get)
-                .responseString(encoding: .utf8) { observer.onNext($0) }
-            return Disposables.create()
-        }
-    }
-    
-    private func toResponseBody(dataResponse: DataResponse<String>) -> Data? {
-        return dataResponse.value?.data(using: .utf8)
     }
     
     private func toStatuses(data: Data?) -> [Status]? {
@@ -43,5 +34,4 @@ class TweetsRepository: BaseTweetsRepository {
         guard let d = data else { return nil }
         return try? decoder.decode([Status].self, from: d)
     }
-    
 }
