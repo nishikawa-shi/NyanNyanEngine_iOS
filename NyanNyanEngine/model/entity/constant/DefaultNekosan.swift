be5c2845b4c46f50651e80ff5ef541c1f3c11399
Tetsuya Nishikawa <tetphi1dev@gmail.com>
2019-05-05 10:59:34 +0900

    ログイン前のデフォルト素材を作成

---
+++ NyanNyanEngine/model/entity/constant/DefaultNekosan.swift
@@ -0,0 +1,51 @@
+//
+//  DefaultNekosanStatus.swift
+//  NyanNyanEngine
+//
+//  Created by Tetsuya Nishikawa on 2019/05/05.
+//  Copyright © 2019 Tetsuya Nishikawa. All rights reserved.
+//
+
+struct DefaultNekosan {
+    let statuses: [Status] = [
+        Status(text: "にゃんにゃにゃおん\n(訳: ここは猫さんの世界)",
+               user: User(name: "ニャン・ニャーンス I世",
+                          screenName: "nyan_nyaaansu")),
+        
+        Status(text: "にゃあにゃおーんにゃ\n(訳: 言葉がみんな猫語になるよ)",
+               user: User(name: "ニャン・ニャーンス I世",
+                          screenName: "nyan_nyaaansu")),
+        
+        Status(text: "にゃんにゃーにゃーお\n(訳: ツイッターでログインして)",
+               user: User(name: "ニャン・ニャーンス I世",
+                          screenName: "nyan_nyaaansu")),
+        
+        Status(text: "にゃにゃんにゃ\n(訳: 一緒に楽しく遊ぼうよ)",
+               user: User(name: "ニャン・ニャーンス I世",
+                          screenName: "nyan_nyaaansu")),
+        
+        Status(text: "🐟",
+               user: User(name: "ニャン・ニャーンス I世",
+                          screenName: "nyan_nyaaansu")),
+        
+        Status(text: "にゃーんにゃーにゃん",
+               user: User(name: "猫一＠起業家",
+                          screenName: "neko_is_the_top")),
+        
+        Status(text: "にゃあにゃんにゃー",
+               user: User(name: "にゃんこグリップ",
+                          screenName: "sutaddoresu_nekosan")),
+        
+        Status(text: "にゃおにゃにゃおーん",
+               user: User(name: "ニャン・ニャーンス I世",
+                          screenName: "nyan_nyaaansu")),
+        
+        Status(text: "にゃあにゃおにゃー",
+               user: User(name: "猫さんドロイド",
+                          screenName: "neko_droid")),
+        
+        Status(text: "にゃーんにゃおにゃー",
+               user: User(name: "ブーツキャット",
+                          screenName: "hosome_zubon"))
+    ]
+}
