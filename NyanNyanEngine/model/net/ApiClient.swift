ade3d20b5d25ba940661eb3b36144a56f8f93233
Tetsuya Nishikawa <tetphi1dev@gmail.com>
2019-05-08 07:05:09 +0900

    未使用メソッドを削除

--- NyanNyanEngine/model/net/ApiClient.swift
+++ NyanNyanEngine/model/net/ApiClient.swift
@@ -10,17 +10,12 @@ import Foundation
 import RxSwift
 
 protocol BaseApiClient: AnyObject {
-    func getResponse(url: String) -> Observable<Data?>
     func postResponse(urlRequest: URLRequest) -> Observable<Data?>
 }
 
 class ApiClient: BaseApiClient {
     static let shared = ApiClient()
     private init() { }
-    
-    func getResponse(url: String) -> Observable<Data?> {
-        return self.getRequest(url: url)
-    }
     
     func postResponse(urlRequest: URLRequest) -> Observable<Data?> {
         return Observable<Data?>.create { observer in
@@ -30,28 +25,5 @@ class ApiClient: BaseApiClient {
                 }.resume()
             return Disposables.create()
         }
-    }
-    
-    private func getRequest(url: String) -> Observable<Data?> {
-        return Observable<Data?>.create { observer in
-            guard let urlRequest = self.createGetUrlRequest(url: url) else { return Disposables.create() }
-            
-            URLSession(configuration: .default)
-                .dataTask(with: urlRequest) { data, _, _ in
-                    DispatchQueue.main.sync { observer.onNext(data) }
-                }.resume()
-            return Disposables.create()
-        }
-    }
-    
-    private func createGetUrlRequest(url: String) -> URLRequest? {
-        guard let urlObj = URL(string: url) else { return nil }
-        
-        var urlRequest = URLRequest(url: urlObj,
-                                    cachePolicy: .reloadIgnoringLocalCacheData,
-                                    timeoutInterval: 5)
-        urlRequest.httpMethod = "GET"
-        
-        return urlRequest
     }
 }
