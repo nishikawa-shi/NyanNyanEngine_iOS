b292199847f78edaaf95780961070268aa099b7d
Tetsuya Nishikawa <tetphi1dev@gmail.com>
2019-05-18 16:18:02 +0900

    テキストフィールドをタップした時、入力中のテキストが全選択されるようにした

--- NyanNyanEngine/ui/postNekogo/PostNekogoViewController.swift
+++ NyanNyanEngine/ui/postNekogo/PostNekogoViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class PostNekogoViewController: UIViewController {
     
+    @IBOutlet weak var originalText: UITextView!
+    
     @IBAction func touchCancelAction(_ sender: UIBarButtonItem) {
         self.dismiss(animated: true, completion: nil)
     }
@@ -18,7 +20,21 @@ class PostNekogoViewController: UIViewController {
         self.dismiss(animated: true, completion: nil)
     }
     
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        self.originalText.delegate = self
+    }
+    
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         self.view.endEditing(true)
+    }
+}
+
+extension PostNekogoViewController: UITextViewDelegate {
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        DispatchQueue.main.async {
+            textView.selectAll(nil)
+        }
     }
 }
