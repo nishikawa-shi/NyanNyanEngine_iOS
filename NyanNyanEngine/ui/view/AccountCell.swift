ff6b91add4cba4bb3c981e33f6fcb1ac0677cb34
Tetsuya Nishikawa <tetphi1dev@gmail.com>
2019-09-23 10:50:36 +0900

    アカウント情報表示用のセルを作成

---
+++ NyanNyanEngine/ui/view/AccountCell.swift
@@ -0,0 +1,15 @@
+//
+//  AccountCell.swift
+//  NyanNyanEngine
+//
+//  Created by Tetsuya Nishikawa on 9/23/19.
+//  Copyright © 2019 Tetsuya Nishikawa. All rights reserved.
+//
+
+import UIKit
+
+class AccountCell: UITableViewCell {
+    @IBOutlet private weak var userImage: UIImageView!
+    @IBOutlet private weak var userName: UILabel!
+    @IBOutlet private weak var userId: UILabel!
+}
