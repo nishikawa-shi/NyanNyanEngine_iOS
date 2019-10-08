0c42241e0367630087dfc5f74f58fd9b107b608a
Tetsuya Nishikawa <tetphi1dev@gmail.com>
2019-10-08 23:56:33 +0900

    タブバーの更新ボタンによって用済みになった、右上の更新ボタンを削除

--- NyanNyanEngine/ui/homeTimeLine/HomeTimelineViewController.swift
+++ NyanNyanEngine/ui/homeTimeLine/HomeTimelineViewController.swift
@@ -36,7 +36,6 @@ class HomeTimelineViewController: UIViewController {
     
     @IBOutlet private weak var navigationBar: UINavigationItem!
     @IBOutlet private weak var authButton: UIBarButtonItem!
-    @IBOutlet private weak var refreshButton: UIBarButtonItem!
     @IBOutlet private weak var tweetList: UITableView!
     @IBOutlet private weak var noticeToast: UILabel!
     @IBOutlet private weak var activityIndicator: UIActivityIndicatorView!
@@ -50,12 +49,6 @@ class HomeTimelineViewController: UIViewController {
             .throttle(DispatchTimeInterval.seconds(3), latest: false, scheduler: ConcurrentMainScheduler.instance)
             .map { "0000/01/01 00:00:00" }
             .bind(to: input.authExecutedAt!)
-            .disposed(by: disposeBag)
-        
-        refreshButton.rx.tap
-            .throttle(DispatchTimeInterval.seconds(3), latest: false, scheduler: ConcurrentMainScheduler.instance)
-            .map { "9999/12/31 23:59:59" }
-            .bind(to: input.buttonRefreshExecutedAt!)
             .disposed(by: disposeBag)
         
         tweetList.rx.itemSelected
