0d8a119382021a86ff7b865473e4d207884fcc65
Tetsuya Nishikawa <tetphi1dev@gmail.com>
2019-10-08 22:48:53 +0900

    タイムライン画面での、タブバーのタイムラインボタンタップイベントを取れるようにした

--- NyanNyanEngine/ui/main/MainViewController.swift
+++ NyanNyanEngine/ui/main/MainViewController.swift
@@ -9,9 +9,30 @@
 import UIKit
 
 class MainViewController: UITabBarController {
+    
+    @IBOutlet weak var mainTabBar: UITabBar!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Do any additional setup after loading the view.
+    }
+    
+    override func tabBar(_ tabBar: UITabBar, didSelect item: UITabBarItem) {
+        if(self.isShowingTimelineView() && item.isTimelineItem()) {
+            print("nyao---n")
+        }
+    }
+}
+
+private extension UITabBarController {
+    func isShowingTimelineView() -> Bool {
+        return self.selectedIndex == 0
+    }
+}
+
+private extension UITabBarItem {
+    func isTimelineItem() -> Bool {
+        return self.title == "Timeline"
     }
 }
