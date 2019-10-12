905ad0d0e883a0e63c131b8c7bfa83e69e59930e
Tetsuya Nishikawa <tetphi1dev@gmail.com>
2019-10-12 22:45:51 +0900

    タブバータップでのスクロールが効かなくなっていた現象を解消

--- NyanNyanEngine/ui/main/MainViewController.swift
+++ NyanNyanEngine/ui/main/MainViewController.swift
@@ -50,6 +50,7 @@ private extension UITabBarController {
 
 private extension UITabBarItem {
     func isTimelineItem() -> Bool {
-        return self.title == "Timeline"
+        //storyboardから、タイムライン用UITabBarItemに1という値をハードコーディングしたのでこっちもそれに合わせた
+        return self.tag == 1
     }
 }
